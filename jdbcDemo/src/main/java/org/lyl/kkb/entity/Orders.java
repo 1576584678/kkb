744ee822802e85e2f2e3131535d230fbebaa9f67
liyuelin <1576584678@qq.com>
2020-07-08 11:17:06 +0800

    添加实体类

---
+++ jdbcDemo/src/main/java/org/lyl/kkb/entity/Orders.java
@@ -0,0 +1,14 @@
+package org.lyl.kkb.entity;
+
+import java.util.Date;
+
+/**
+ * @Description
+ * @Author liyuelin
+ * @Date 2020/7/8
+ */
+public class Orders {
+    private int orderno;
+    private Date createdate;
+    private int userid;
+}
---
+++ jdbcDemo/src/main/java/org/lyl/kkb/entity/User.java
@@ -0,0 +1,15 @@
+package org.lyl.kkb.entity;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author liyuelin
+ * @Date 2020/7/8
+ */
+@Data
+public class User {
+    private int id;
+    private String username;
+    private String sex;
+}
